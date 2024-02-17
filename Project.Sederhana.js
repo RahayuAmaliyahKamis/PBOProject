42638f00776cee26b9bab3e910c5d350073667ea
RahayuAmaliyahKamis <160306918+RahayuAmaliyahKamis@users.noreply.github.com>
2024-02-17 13:45:48 +0700

    Tugas Sesi 1

---
+++ Project.Sederhana.js
@@ -0,0 +1,23 @@
+
+
+// Identitas Anda
+let nama = "Rahayu Amaliyah Kamis";
+let nim = "20220040248";
+
+// Operasi aritmatika sederhana
+let A1 = 20;
+let A2 = 15;
+
+let Tambah = A1 + A2;
+let Kurang = A1 - A2;
+let Kali = A1 * A2;
+let Bagi = A1 / A2;
+
+// Menampilkan identitas dan hasil operasi aritmatika
+console.log(`Nama: ${nama}`);
+console.log(`NiM: ${nim}`);
+console.log(`Hasil Penambahan: ${Tambah}`);
+console.log(`Hasil Pengurangan: ${Kurang}`);
+console.log(`Hasil Perkalian: ${Kali}`);
+console.log(`Hasil Pembagian: ${Bagi}`);
+
